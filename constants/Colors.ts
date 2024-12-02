be4d5261c150a7d1a6fda0083535eb5bb8894193
Samit Koyom <samitkoyom@gmail.com>
2024-12-03 00:50:44 +0700

    initial commit

---
+++ constants/Colors.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  // โหมดสว่าง
+  light: {
+    text: '#fff', // สีตัวอักษร
+    background: '#161622', // สีพื้นหลัง
+  },
+  // โหมดมืด
+  dark: {
+    text: '#fff', // สีตัวอักษร
+    background: '#161622', // สีพื้นหลัง
+  },
+}
+
+export default Colors
